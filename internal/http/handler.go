d6345b7513bd794a6ba7b8b52341b1853123d424
Saurabh Galinde <srbhgalinde@gmail.com>
2025-09-15 23:26:33 +0530

    Updated handler to store mappings of shortened and original  url

--- internal/http/handler.go
+++ internal/http/handler.go
@@ -9,26 +9,51 @@ import (
 	"github.com/srbhgalinde/url-shortner/internal/models"
 )
 
-const Baseurl = "http://localhost:8080/"
+const (
+	Baseurl = "http://localhost:8080/"
+)
+
+var (
+	urlStore     = make(map[string]string) // backhalf -> originalURL
+	reverseStore = make(map[string]string) // originalURL -> backhalf
+)
 
 func shortenURLHandler(c *gin.Context) {
 	var req models.ShortenRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
 		return
 	}
 
-	id := uuid.New().String()[:8]
-
-	backhalf := req.Backhalf
-	if backhalf == "" {
-		backhalf = id
+	// If URL was already shortened, return respective backhalf
+	if existing, ok := reverseStore[req.URL]; ok {
+		shortURL := fmt.Sprintf("%s/%s", Baseurl, existing)
+		c.JSON(http.StatusOK, gin.H{
+			"shortUrl": shortURL,
+			"backhalf": existing,
+		})
+		return
 	}
 
-	shortURL := fmt.Sprintf("%s/%s", Baseurl, backhalf)
+	// eles, generate new backhalf
+	backhalf := req.Backhalf
+	if backhalf == "" {
+		backhalf = uuid.New().String()[:8]
+	}
 
+	// Ensure backhalf is unique
+	if _, exists := urlStore[backhalf]; exists {
+		c.JSON(http.StatusConflict, gin.H{"error": "backhalf already taken"})
+		return
+	}
+
+	// Save mappings
+	urlStore[backhalf] = req.URL
+	reverseStore[req.URL] = backhalf
+
+	shortURL := fmt.Sprintf("%s/%s", Baseurl, backhalf)
 	c.JSON(http.StatusCreated, gin.H{
-		"id":       id,
 		"shortUrl": shortURL,
+		"backhalf": backhalf,
 	})
 }
